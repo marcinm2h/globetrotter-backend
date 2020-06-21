4be267cf020efd0e29c20ca516831849d27de3aa
Marcin Moch <marcio.moch@gmail.com>
2020-06-21 15:02:43 +0200

    lowdb

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const low = require("lowdb");
+const FileAsync = require("lowdb/adapters/FileAsync");
 const { router } = require("./router");
 const { errorHandler } = require("./auth");
 
@@ -11,15 +13,17 @@ const logger = () => (req, res, next) => {
 };
 
 const PORT = process.env.PORT || 8080;
+const DB_FILE_PATH = process.env.DB_FILE_PATH || "db.json";
 
 const app = express();
 
+low(new FileAsync(DB_FILE_PATH)).then((db) => {
+  app.use(bodyParser.json());
+  app.use(logger());
+  app.use(router(db));
+  app.use(errorHandler());
 
-app.use(bodyParser.json());
-app.use(logger());
-app.use(router);
-app.use(errorHandler());
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 });
--- src/router.js
+++ src/router.js
@@ -1,40 +1,49 @@
 const { Router } = require("express");
 const { auth, createToken } = require("./auth");
 
-const router = Router();
+const router = (db) => {
+  const router = Router();
 
-router.get("/", (req, res) => {
-  res.send({
-    status: 200,
-    data: {
-      message: "Api root",
-    },
+  router.get("/", (req, res) => {
+    db.get("users").push({ id: 1, name: "Marcin" }).write();
+
+    res.send({
+      status: 200,
+      data: {
+        message: "Api root",
+      },
+    });
   });
-});
 
-router.post("/login", (req, res) => {
-  const user = {
-    login: "marcin",
-  };
-  const token = createToken(user);
+  router.post("/login", (req, res) => {
+    const user = {
+      login: "marcin",
+    };
+    const token = createToken(user);
 
-  res.send({
-    status: 200,
-    data: {
-      token,
-    },
+    res.send({
+      status: 200,
+      data: {
+        token,
+      },
+    });
   });
-});
 
-router.get("/secure", auth, (req, res) => {
-  console.log("/secure");
-  res.send({
-    status: 200,
-    data: {
-      ...req.user,
-      message: "secure",
-    },
+  router.get("/secure", auth, (req, res) => {
+    console.log("/secure");
+    res.send({
+      status: 200,
+      data: {
+        ...req.user,
+        message: "secure",
+      },
+    });
   });
-});
+
+  router.get("/places", auth, () => {});
+  router.get("/places", auth, () => {});
+
+  return router;
+};
 
 module.exports = { router };
